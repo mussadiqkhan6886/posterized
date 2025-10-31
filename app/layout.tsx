96a260ae70847842b445afeab203e4555cd54da6
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-31 21:20:34 +0500

    fixed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { bricolage } from "@/lib/fonts/font";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={` ${bricolage.className} antialiased`}
       >
         {children}
       </body>
---
+++ lib/fonts/font.ts
@@ -0,0 +1,10 @@
+import {Bricolage_Grotesque, Roboto} from "next/font/google"
+
+export const bricolage = Bricolage_Grotesque({
+    weight: ["200","400", "500", "600", "700", "800"],
+    subsets: ["latin"]
+})
+export const roboto = Roboto({
+    weight: ["100", "200", "300","400", "500", "600", "700", "800"],
+    subsets: ["latin"]
+})
