2647d57abde76248a46e984db7a7f5c9a96663e9
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-01 02:17:17 +0500

    custom done

--- components/Explore.tsx
+++ components/Explore.tsx
@@ -5,17 +5,25 @@ import React from 'react'
 
 const Explore = () => {
   return (
-    <section className='max-w-[1240px] mx-auto'>
+    <section className='max-w-[1240px] mx-auto my-10'>
       <h3 className="font-bold text-5xl mb-10 text-center">Explore</h3>
       <div className='grid grid-cols-3 gap-6'>
         {exploreSections.map(item => (
             <div key={item.title}>
-                <Image src={item.image} alt={item.title} width={400} height={400} className='object-center object-cover' />
+                <Image src={item.image} alt={item.title} width={400} height={400} className='object-center object-cover cursor-pointer hover:saturate-100 saturate-50 duration-200 ' />
                 <h4 className='text-2xl mt-5 font-medium text-black'>{item.title}</h4>
                 <p className='text-font text-[14px] leading-tight mt-2'>{item.description}</p>
-                <Link className='border-b border-black/50 font-semibold text-lg pb-1 mt-2' href={"/"}>{item.button}</Link>
+                <Link className='border-b border-black/50 font-semibold text-lg pb-1 mt-4' href={"/"}>{item.button}</Link>
             </div>
         ))}
+      </div>
+      <div className='relative my-10'>
+        <Image src={"/custom.jpg"} alt='custom image' width={1000} height={1000} className='w-full h-full object-center object-cover hover:brightness-75 duration-300 brightness-90' />
+        <div className='absolute bottom-10 text-white left-10'>
+            <h4 className='text-5xl font-bold'>Design Your Own</h4>
+            <p className='font-base font-medium my-2 max-w-xl tracking-tight'>With our visualization tool, you can experiment endlessly with colors, sizes and layouts until you have designed the art of your dreams</p>
+            <Link href='/' className='border-b border-white pb-1 font-semibold text-lg' >Try Now</Link>
+        </div>
       </div>
     </section>
   )
