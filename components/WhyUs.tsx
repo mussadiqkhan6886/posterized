38f85dda2a32294d4bd2be7c4438ff333805f1bb
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-01 19:24:33 +0500

    dine

--- components/WhyUs.tsx
+++ components/WhyUs.tsx
@@ -8,20 +8,31 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { FiStar } from 'react-icons/fi';
+import { delay, motion } from 'framer-motion';
 
 const WhyUs = () => {
+    const variant = {
+        hidden: {
+            y: 100,
+            opacity:0
+        },
+        show: {
+            y:0,
+            opacity:1
+        }
+    }
   return (
     <section className="my-20 max-w-[1240px] px-10 mx-auto">
         <h3 className={`text-4xl md:text-5xl mb-8 text-center`}>Why Choose Us</h3>
         <div className='grid lg:grid-cols-4 gap-5 md:grid-cols-2 grid-cols-1 my-10'>
             {whyUs.map((item, i) => (
-                <div className='shadow-xl flex items-center flex-col  p-3' key={i}>
+                <motion.div variants={variant} whileInView={"show"} initial="hidden"   className='shadow-xl flex items-center flex-col  p-3' key={i}>
                     <item.icon className='text-4xl' />
                     <div>
                         <p className='font-semibold text-lg text-center mb-1'>{item.title}</p>
                         <p className='text-font text-sm text-center'>{item.description}</p>
                     </div>
-                </div>
+                </motion.div>
             ))}
         </div>
         <h3 className={`text-5xl my-14 text-center`}>Testimonials</h3>
